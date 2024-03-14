2f6330ccddf41fe59f861ab880c8b6787c2df322
Всеволод <sevateti@yandex.com>
2024-03-14 23:40:38 +0300

    Добавьте файлы проекта.

---
+++ App_Code/IService.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    [WebGet(UriTemplate = "GetData?value={value}", ResponseFormat = WebMessageFormat.Json)]
+    string GetData(int value);
+
+    [OperationContract]
+    [WebInvoke(UriTemplate = "GetDataUsingDataContract", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+    CompositeType GetDataUsingDataContract(CompositeType composite);
+}
+
+[DataContract]
+public class CompositeType
+{
+    bool boolValue = true;
+    string stringValue = "Hello ";
+
+    [DataMember]
+    public bool BoolValue
+    {
+        get { return boolValue; }
+        set { boolValue = value; }
+    }
+
+    [DataMember]
+    public string StringValue
+    {
+        get { return stringValue; }
+        set { stringValue = value; }
+    }
+}
---
+++ App_Code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace MyWcfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/MyService");
+
+            using (ServiceHost host = new ServiceHost(typeof(Service), baseAddress))
+            {
+                // Добавляем конечную точку (endpoint)
+                host.AddServiceEndpoint(typeof(IService), new BasicHttpBinding(), "");
+
+                // Открываем хост
+                host.Open();
+
+                Console.WriteLine("Сервис запущен. Нажмите любую клавишу для завершения.");
+                Console.ReadKey();
+
+                // Закрываем хост
+                host.Close();
+            }
+        }
+    }
+}
